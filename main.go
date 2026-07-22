5533f9bad01a9cbe5cc9d8ccf8d042cc4b94f27a
sharipovr <sharipov.rustem@gmail.com>
2026-07-21 20:10:25 -0700

    Add CRUD api endpoints (without PATCH)

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
 )
 
 type ShoppingList struct {
@@ -13,5 +16,72 @@ type ShoppingList struct {
 var allData []ShoppingList
 
 func main() {
+	http.HandleFunc("POST /v1/lists", handleCreateList)
+	http.HandleFunc("GET /v1/lists", handleListLists)
+	http.HandleFunc("DELETE /v1/lists/{id}", handleDeleteList)
+	http.HandleFunc("PUT /v1/lists/{id}", handleUpdateList)
+	fmt.Println("listening on port :8888")
 	http.ListenAndServe(":8888", nil)
+}
+
+func handleCreateList(w http.ResponseWriter, r *http.Request) {
+	var list ShoppingList
+	err := json.NewDecoder(r.Body).Decode(&list)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	allData = append(allData, list)
+	w.WriteHeader(http.StatusCreated)
+	err = json.NewEncoder(w).Encode(list)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func handleListLists(w http.ResponseWriter, r *http.Request) {
+	data, err := json.Marshal(allData)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	_, err = w.Write(data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func handleDeleteList(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	for i, list := range allData {
+		if strconv.Itoa(list.ID) == id {
+			allData = append(allData[:i], allData[i+1:]...)
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+	}
+	http.Error(w, "List not found", http.StatusNotFound)
+}
+
+func handleUpdateList(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	for i, list := range allData {
+		if strconv.Itoa(list.ID) == id {
+			var updatedList ShoppingList
+			err := json.NewDecoder(r.Body).Decode(&updatedList)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			allData[i] = updatedList
+			if err := json.NewEncoder(w).Encode(updatedList); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			return
+		}
+	}
+	http.Error(w, "List not found", http.StatusNotFound)
 }
