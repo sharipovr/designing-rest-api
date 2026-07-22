00ffe7d8466f7925de2b1e68e7fe83a9e212a6fb
sharipovr <sharipov.rustem@gmail.com>
2026-07-21 21:39:18 -0700

    Add PATCH and GET a single resource endpoints

--- main.go
+++ main.go
@@ -15,11 +15,18 @@ type ShoppingList struct {
 
 var allData []ShoppingList
 
+type ShoppingListPatch struct {
+	Name  *string  `json:"name"`
+	Items []string `json:"items"`
+}
+
 func main() {
 	http.HandleFunc("POST /v1/lists", handleCreateList)
 	http.HandleFunc("GET /v1/lists", handleListLists)
 	http.HandleFunc("DELETE /v1/lists/{id}", handleDeleteList)
 	http.HandleFunc("PUT /v1/lists/{id}", handleUpdateList)
+	http.HandleFunc("PATCH /v1/lists/{id}", handlePatchList)
+	http.HandleFunc("GET /v1/lists/{id}", handleGetList)
 	fmt.Println("listening on port :8888")
 	http.ListenAndServe(":8888", nil)
 }
@@ -77,6 +84,54 @@ func handleUpdateList(w http.ResponseWriter, r *http.Request) {
 			}
 			allData[i] = updatedList
 			if err := json.NewEncoder(w).Encode(updatedList); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			return
+		}
+	}
+	http.Error(w, "List not found", http.StatusNotFound)
+}
+
+func handlePatchList(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	for i, list := range allData {
+		if strconv.Itoa(list.ID) == id {
+			var patch ShoppingListPatch
+			err := json.NewDecoder(r.Body).Decode(&patch)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			if patch.Name != nil {
+				list.Name = *patch.Name
+			}
+			if patch.Items != nil {
+				list.Items = patch.Items
+			}
+			allData[i] = list
+			err = json.NewEncoder(w).Encode(list)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			return
+		}
+	}
+	http.Error(w, "List not found", http.StatusNotFound)
+}
+
+func handleGetList(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	for _, list := range allData {
+		if strconv.Itoa(list.ID) == id {
+			data, err := json.Marshal(list)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			_, err = w.Write(data)
+			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
